2fe2cd2658a7922a4866f224802894d9b0af7220
ashaidi1@stu.vistula.edu.pl <ashaidi1@stu.vistula.edu.pl>
2022-03-09 14:32:37 +0100

    Initial commit

---
+++ src/WeightOnMars.java
@@ -0,0 +1,28 @@
+public class WeightOnMars {
+
+    public static void main(String[] args) {
+        float earthWeight;
+        float marsWeight;
+        double marsWeightInDouble;
+        int marsWeightInInt;
+
+        earthWeight = 84;
+        marsWeight = (earthWeight * 0.38F);
+        System.out.println(earthWeight + " kg on Earth is " + marsWeight + "kg on Mars");
+
+        marsWeightInDouble = marsWeight;
+        System.out.println("Kilograms on Mars after converting to double " + marsWeightInDouble);
+
+        System.out.printf("Kilograms on Mars displayed to four decimal places: %.4f %n", marsWeightInDouble);
+
+        marsWeightInInt = (int)marsWeightInDouble;
+        System.out.println("Kilograms on Mars when casted to integer " + marsWeightInInt);
+
+        char c  = (char)marsWeightInInt;
+        System.out.println("The ASCII table equivalent of marsWeightInInt is: " + c);
+
+        int exampleOfMathOnChar = c* c;
+        System.out.println("An example of a mathematical operation on the char type: "+ exampleOfMathOnChar);
+
+    }
+}
